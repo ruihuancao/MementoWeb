afc3077dc0127c5ff2681520cd49e6c4f01e1e00
ruihuancao <ruihuancao@gmail.com>
2016-11-29 17:26:45 +0800

    init

--- app/__init__.py
+++ app/__init__.py
@@ -10,20 +10,20 @@ from .react import react
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config.default')
 app.config.from_pyfile('config.py')
-app.config.from_envvar('APP_CONFIG_FILE')
+#app.config.from_envvar('APP_CONFIG_FILE')
 
 
-# @app.route('/')
-# @app.route('/<name>')
-# def index(name=None):
-#     item = {'url':'http://placehold.it/400x300', 'alt':'test'}
-#     data = [item for i in range(100)]
-#     title = 'test'
-#     return render_template('index.html', name=name, data = data, title=title)
-#
-#
-# app.register_blueprint(home, url_prefix='/home')
-# app.register_blueprint(react, url_prefix='/react')
+@app.route('/')
+@app.route('/<name>')
+def index(name=None):
+    item = {'url':'http://placehold.it/400x300', 'alt':'test'}
+    data = [item for i in range(100)]
+    title = 'test'
+    return render_template('index.html', name=name, data = data, title=title)
+
+
+app.register_blueprint(home, url_prefix='/home')
+app.register_blueprint(react, url_prefix='/react')
 
 api = Api(app)
 api.add_resource(TodoList, '/todo/api/v1.0/todos', endpoint = 'todos')
